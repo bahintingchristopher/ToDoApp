fdf6999899967767850a6933bb4800e8a1200022
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-06-24 11:02:59 +0800

    explicitly pin controllers entry point

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 
-# Explicitly map the 'to' target so production builds know exactly where the map points
-pin_all_from "app/javascript/controllers", under: "controllers", to: "controllers"
+# Add this explicit pin so the application knows what "controllers" means
+pin "controllers", to: "controllers/index.js"
+
+pin_all_from "app/javascript/controllers", under: "controllers"
