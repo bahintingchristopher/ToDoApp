680a9c2e1343027cee1b23f7e6263f5145d47b8f
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-06-24 11:43:40 +0800

    fix: pin hotwired stimulus packages in importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,12 @@
+# Pin npm packages by running ./bin/importmap
+
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 
-# Add this explicit pin so the application knows what "controllers" means
-pin "controllers", to: "controllers/index.js"
+# Add these two pins for Stimulus so the application can resolve "@hotwired/stimulus"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
+# This maps the controllers folder entry point
+pin "controllers", to: "controllers/index.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
