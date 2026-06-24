4c40f2e0bf9d4d82db101c2bfee71fb74827f55a
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-06-24 10:13:48 +0800

    fix importmap controllers target path

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,6 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Explicitly map the 'to' target so production builds know exactly where the map points
+pin_all_from "app/javascript/controllers", under: "controllers", to: "controllers"
